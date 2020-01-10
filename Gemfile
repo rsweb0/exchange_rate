2af99a480fd3163a333de990f756ce26dfb6909f
Rohit <rswebstar267@gmail.com>
2020-01-10 10:33:48 +0530

    Configure rspec

--- Gemfile
+++ Gemfile
@@ -28,6 +28,12 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry'
+  gem 'rspec-rails'
+  gem 'pry-byebug'
+  gem 'shoulda-matchers'
+  gem 'database_cleaner'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 group :development do
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'factory_bot_rails'
+require 'shoulda-matchers'
+require 'database_cleaner'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+  end
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.include FactoryBot::Syntax::Methods
+end
